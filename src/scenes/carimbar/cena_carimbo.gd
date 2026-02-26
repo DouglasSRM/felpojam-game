5b03e1bc2eb5492d10fc42c08cd9b0e8157e0e04
Douglas Seramim <douglasfseramim@gmail.com>
2026-02-25 22:52:49 -0300

    sons

--- src/scenes/carimbar/cena_carimbo.gd
+++ src/scenes/carimbar/cena_carimbo.gd
@@ -4,17 +4,8 @@ extends Node2D
 
 func _ready() -> void:
 	var carimbar: Node2D
-	var cena: String
-	if SceneManager.scene_stack[0].name == "Cena2":
-		cena = "carimbar_1"
-	elif SceneManager.scene_stack[0].name == "Cena4":
-		cena = "carimbar_2"
-	else:
-		await Utils.sleep(0.1)
-		terminou()
-		return
 	
-	carimbar = load("res://src/scenes/carimbar/"+cena+".tscn").instantiate()
+	carimbar = load("res://src/scenes/carimbar/carimbar_1.tscn").instantiate()
 	add_child(carimbar)
 	carimbar.terminou.connect(Callable(self, "terminou"))
 
